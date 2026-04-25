ea32d540c478c86d02c7a6421517621a4e5c721e
Piyush Aggarwal <piyushagwl007@gmail.com>
2026-04-25 19:30:38 +0530

    75. Sort Colors

--- solutions/0075-sort-colors/solution.py
+++ solutions/0075-sort-colors/solution.py
@@ -17,11 +17,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        counts = [0, 0, 0]
-        for num in nums:
-            counts[num] += 1
-        index = 0
-        for i in range(len(counts)):
-            for _ in range(counts[i]):
-                nums[index] = i
-                index += 1
+        low = 0
+        mid = 0
+        high = len(nums)-1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[mid] = nums[low]
+                nums[low] = 0
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid] = nums[high]
+                nums[high] = 2
+                high -= 1
+            
